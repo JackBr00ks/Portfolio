c62441d279a324be4113df14c5e7b74074b350ec
JackBr00ks <jack.brooks@live.com>
2023-07-26 18:45:47 +0200

    Added png files and icons to Notepad.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,33 +1,48 @@
-import React from 'react'
-import styled from 'styled-components'
-import '@react95/icons/icons.css';
+import React from 'react';
+import styled from 'styled-components';
+
 
 const StyledItem = styled.div`
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	flex-direction: column;
-	text-align: center;
-	width: 25%;
-	padding: 10px 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  width: 25%;
+  padding: 10px 0;
 `;
 
 const StyledSpan = styled.span`
-	margin-top: 5px;
-`
+  margin-top: 5px;
+`;
 
 function File({ item, openNotepad }) {
-	const {name, icon } = item;
-	return (
-		<StyledItem>
-			<img
-				name={icon}
-				className="pointer"
-				onClick={() => openNotepad(item)}
-			/>
-			<StyledSpan>{name}</StyledSpan>
-		</StyledItem>
-	)
+  const { name, icon } = item;
+
+  // Map icon names to their respective image paths in the src/assets folder
+  const iconImagePath = {
+    info_bubble: require('../assets/InfoBubble_32x32_4.png'),
+    notepad_2: require('../assets/Notepad2_32x32_4.png'),
+    flying_through_space_100: require('../assets/FlyingThroughSpace100_32x32_4.png'),
+    progman_11: require('../assets/Progman11_32x32_4.png'),
+    inetcfg_2301: require('../assets/Inetcfg2301_32x32_4.png'),
+    // Add more icon mappings here as needed
+  };
+
+  // Use the custom icon image path if available, otherwise fallback to Icon component
+  const iconImage = iconImagePath[icon] || icon;
+
+  return (
+    <StyledItem>
+      <img
+        src={iconImage}
+        alt={icon} // Use the icon name as alt text
+        className="pointer"
+        onClick={() => openNotepad(item)}
+      />
+      <StyledSpan>{name}</StyledSpan>
+    </StyledItem>
+  );
 }
 
-export default File
+export default File;
